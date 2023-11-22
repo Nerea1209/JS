2a6ec3a9c8e50288f29692ba1c1163267bb22f85
Nerea1209 <nlopsan0104@g.educaand.es>
2023-11-22 12:48:36 +0100

    Termina saluda

--- REACT/saluda/src/App.js
+++ REACT/saluda/src/App.js
@@ -2,28 +2,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Button } from 'reactstrap';
 
+function Boton(props) {
+  return (
+    <Button color={props.color} onClick={props.cambia}>
+      {props.texto}
+    </Button>
+  )
+}
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      letrero: "saludo en diferentes idiomas",
+    }
+  }
+  cambia({ saludo }) {
+    this.setState({ letrero: saludo })
+  }
+
   render() {
     return (
       <div className="App">
-        <Button color="success">
-          Inglés
-        </Button>
-        {' '}
-        <Button color="primary">
-          Aleman
-        </Button>
-        {' '}
-        <Button>
-          Sueco
-        </Button>
-        {' '}
-        <Button color="danger">
-          Español
-        </Button>
+        <h1>{this.state.letrero}</h1>
+        <Boton cambia={() => this.cambia({saludo: "Hello!"})} texto="Inglés" color="danger" />
+        <Boton cambia={() => this.cambia({saludo: "Salut!"})} texto="Francés" color="info" />
+        <Boton cambia={() => this.cambia({saludo: "¡Hola!"})} texto="Español" color="success" />
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+cambia() {
+if (this.state.color === "danger") {
+this.setState({ color: "success" })
+} else {
+this.setState({ color: "danger" })
+}
+}
+*/
