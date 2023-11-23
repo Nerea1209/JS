8c5688e6dc78c69702532cd16197bfd8167868b8
Nerea1209 <nlopsan0104@g.educaand.es>
2023-11-23 12:48:49 +0100

    Termina semaforo

---
+++ REACT/semaforo/src/App.js
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button, Spinner } from 'reactstrap';
+
+function Botoncico (props) {
+  return (
+    <Button color={props.color} outline onClick={props.cambia}>{props.value}</Button>
+  );
+}
+
+function Spinico (props) {
+  return (
+    <Spinner color={props.color} type="grow" />
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "primary",
+    }
+  }
+  cambia({ c }) {
+    this.setState({color: c})
+  }
+  render() {
+    return (
+      <div className="App">
+        <Botoncico color="primary" value="marinico" cambia={() => this.cambia({c: "primary"})} />
+        <Botoncico color="secondary" value="grisico" cambia={() => this.cambia({c: "secondary"})}/>
+        <Botoncico color="success" value="verdecico" cambia={() => this.cambia({c: "success"})}/>
+        <Botoncico color="info" value="azulico" cambia={() => this.cambia({c: "info"})}/>
+        <Botoncico color="warning" value="amarillico" cambia={() => this.cambia({c: "warning"})}/>
+        <Botoncico color="danger" value="rojico" cambia={() => this.cambia({c: "danger"})}/> <br />
+        <Spinico color={this.state.color} />
+      </div>
+    );    
+  }
+}
+
+export default App;
