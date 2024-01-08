1642921fb323480270f54481fba2737bc3c935f3
Nerea1209 <nlopsan0104@g.educaand.es>
2024-01-08 12:50:42 +0100

    Termina minevancic sin comprobar caminos

--- REACT/react/src/componentes/SelectorFilasColumnas.js
+++ REACT/react/src/componentes/SelectorFilasColumnas.js
@@ -4,25 +4,25 @@ import { Button } from 'reactstrap';
 
 function Selector(props) {
   return (
-    <section style={{ width: '200px', marginTop:'1rem'}}>
+    <section style={{ width: '200px', marginTop: '1rem' }}>
       <article style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{margin:'.2rem 0'}}>
+        <div style={{ margin: '.2rem 0' }}>
           <span style={{ fontSize: "1rem" }}>Filas:</span>
           <span>{props.filas}</span>
         </div>
-        <div style={{margin:'.2rem 0'}}>
+        <div style={{ margin: '.2rem 0' }}>
           <Button style={{ width: "2rem", height: "2rem", lineHeight: "1rem" }} color='primary' outline onClick={() => props.onClickMenos("filas")}>-</Button>
           <Button style={{ width: "2rem", height: "2rem", lineHeight: "1rem", padding: 0 }} color='secondary' outline onClick={() => props.onClickMas("filas")}>+</Button>
         </div>
       </article>
       <article style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{margin:'.2rem 0'}}>
+        <div style={{ margin: '.2rem 0' }}>
           <span style={{ fontSize: "1rem" }}>Columnas:</span>
-          <span>{props.filas}</span>
+          <span>{props.columnas}</span>
         </div>
-        <div style={{margin:'.2rem 0'}}>
-          <Button style={{ width: "2rem", height: "2rem", lineHeight: "1rem" }} color='primary' outline onClick={() => props.onClickMenos("filas")}>-</Button>
-          <Button style={{ width: "2rem", height: "2rem", lineHeight: "1rem", padding: 0 }} color='secondary' outline onClick={() => props.onClickMas("filas")}>+</Button>
+        <div style={{ margin: '.2rem 0' }}>
+          <Button style={{ width: "2rem", height: "2rem", lineHeight: "1rem" }} color='primary' outline onClick={() => props.onClickMenos("columnas")}>-</Button>
+          <Button style={{ width: "2rem", height: "2rem", lineHeight: "1rem", padding: 0 }} color='secondary' outline onClick={() => props.onClickMas("columnas")}>+</Button>
         </div>
       </article>
     </section>
