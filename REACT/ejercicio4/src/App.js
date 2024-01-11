4857372b3382664556d1aff8e13f1603e9aad395
Nerea1209 <nlopsan0104@g.educaand.es>
2024-01-11 19:42:39 +0100

    Termina ejercicio 4

--- REACT/ejercicio4/src/App.js
+++ REACT/ejercicio4/src/App.js
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserList from './componentes/UserList';
 import UserForm from './componentes/UserForm';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component{
-  constructor (props) {
-    super(props);
+class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
       users: [
-        {id: 1, name:"perico", email: "perico@myfpschool.com"},
-        {id: 2, name:"juanico", email: "juanico@myfpschool.com"},
-        {id: 3, name:"andrés", email: "andres@myfpschool.com"}
-      ],
-    }
+        { id: 1, name: "perico", email: "perico@myfpschool.com" },
+        { id: 2, name: "juanico", email: "juanico@myfpschool.com" },
+        { id: 3, name: "andrés", email: "andrés@myfpschool.com" }
+      ]
+    };
   }
 
-  handlerOnAddUser(event){
+  handleOnAddUser(event) {
     event.preventDefault();
-    console.log(event.target.name.value + " " + event.target.email.value);
+    let user = {
+      name: event.target.name.value,
+      email: event.target.email.value
+    };
+    let tmp = this.state.users;
+    tmp.push(user);
+    this.setState({
+      users: tmp
+    });
   }
-
-  render () {
+  render() {
     return (
       <div className="App">
-        <h2>Me mola Myfpschool</h2>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Me mola Myfpschool</h2>
+        </div>
         <div>
           <p><strong>Añade usuarios</strong></p>
           <UserList users={this.state.users} />
+          <UserForm onAddUser={(e) => this.handleOnAddUser(e)} />
         </div>
-        <UserForm onAddUsser={(e) => this.handlerOnAddUser(e)} />
       </div>
     );
   }
 }
-
 export default App;
