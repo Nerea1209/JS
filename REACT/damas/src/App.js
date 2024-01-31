fcefc859b9c85b6f5da4aac6d358913169ffd54e
Nerea1209 <nlopsan0104@g.educaand.es>
2024-01-31 10:44:16 +0100

    Crea la funcion colorear

--- REACT/damas/src/App.js
+++ REACT/damas/src/App.js
@@ -16,7 +16,7 @@ function Botonera(props) {
         if (elemento.split(",")[2] === "secondary")
           return <Col key={i + "" + j} style={{ padding: 0 }}><Button style={{ width: '100%', height: '100%' }} color={elemento.split(",")[2]}></Button></Col>
         else
-          return <Col key={i + "" + j} style={{ padding: 0 }}><Button style={{ width: '100%', height: '100%' }} color={elemento.split(",")[2]} onClick={() => props.setSeleccionado(i, j)}></Button></Col>
+          return <Col key={i + "" + j} style={{ padding: 0 }}><Button style={{ width: '100%', height: '100%' }} color={elemento.split(",")[2]} onClick={() => props.colorear(i, j)}></Button></Col>
       } else {
         return <Col key={i + "" + j} style={{ padding: 0 }}><Button style={{ width: '100%', height: '100%' }} color="secondary" outline></Button></Col>
       }
@@ -30,13 +30,53 @@ class App extends Component {
     this.state = {
       //DEFINE EL ESTADO DE TU COMPONENTE PRINCIPAL
       //Recuerda que si defines una tabla 8x8 tu estado será inválido.
-      tabla: Array(8),
-      seleccionado: []
+      tabla: Array(4),
     };
   }
 
-  setSeleccionado(i, j) {
-    this.setState({ seleccionado: [i, j] })
+  puedeMover(f, c) {
+    let aux = JSON.parse(JSON.stringify(this.state.tabla));
+    let elemento = aux.find((fila, i) => fila.find((columna, j) => columna.split(",")[0] == f && columna.split(",")[1] == c));
+    if (elemento.split(",")[2] === "danger") {
+      if (f % 2 === 0) { // par
+
+      } else { // impar
+
+      }
+    }
+  }
+
+  colorear(f, c) {
+    console.log(this.state.tabla)
+    let aux = JSON.parse(JSON.stringify(this.state.tabla));
+    let tableroEntero = Array(8).fill(Array(8).fill(null));
+    const divisor = 2;
+    tableroEntero.forEach((fila, i) => {
+      let array = [];
+      fila.forEach((columna, j) => {
+        if (i == f && j == c) {
+          array.push(i + "," + j + ",danger")
+        } else if (i == 0 || i % divisor === 0) {
+          if (j % divisor !== 0) {
+            if (i < 5) {
+              array.push(i + "," + j + ",secondary")
+            } else {
+              array.push(i + "," + j + ",success")
+            }
+          }
+        } else {
+          if (j % divisor === 0) {
+            if (i < 5) {
+              array.push(i + "," + j + ",secondary")
+            } else {
+              array.push(i + "," + j + ",success")
+            }
+          }
+        }
+      })
+      aux.push(array);
+    })
+    this.setState({ tabla: aux.filter(v => v != null) });
   }
 
   componentWillMount() {
@@ -47,9 +87,7 @@ class App extends Component {
     tableroEntero.forEach((fila, i) => {
       let array = [];
       fila.forEach((columna, j) => {
-        if (i == this.state.seleccionado[0] && j == this.state.seleccionado[1]) {
-          array.push(i + "," + j + ",danger")
-        } else if (i == 0 || i % divisor === 0) {
+        if (i == 0 || i % divisor === 0) {
           if (j % divisor !== 0) {
             if (i < 5) {
               array.push(i + "," + j + ",secondary")
@@ -77,7 +115,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           {/* //AQUÍ TIENES QUE RENDERIZAR EL COMPONENTE BOTONERA */}
-          <Botonera tabla={this.state.tabla} setSeleccionado={(i, j) => this.setSeleccionado(i, j)} />
+          <Botonera tabla={this.state.tabla} colorear={(i, j) => this.colorear(i, j)} />
         </header>
       </div>
     );
