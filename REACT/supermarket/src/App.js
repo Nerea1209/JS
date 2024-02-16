c3d0e0c43bc51e929066f9711e571193f85d9bdb
Nerea1209 <nlopsan0104@g.educaand.es>
2024-02-16 11:14:58 +0100

    Empieza supermarket

---
+++ REACT/supermarket/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+function Mapa(props) {
+  return (
+    <Container>
+
+      {props.poblacion.map((v, i) => {
+        if (i % 9 === 0) {
+          return (
+            <Row xs="9">
+              <Col className="bg-light border">{v}</Col>
+            </Row>
+          )
+
+        } else {
+          return <Col className="bg-light border">{v}</Col>
+        }
+      })}
+    </Container>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      poblacion: [0, 5, 4, 2, 9, 8, 0, 8, 8,
+        1, 7, 21, 23, 44, 5, 3, 4, 0,
+        2, 6, 32, 22, 33, 8, 4, 2, 8,
+        1, 2, 43, 4, 56, 65, 34, 11, 8,
+        2, 22, 32, 3, 42, 62, 43, 21, 0,
+        2, 2, 23, 34, 64, 24, 42, 15, 7,
+        0, 2, 36, 43, 61, 26, 64, 12, 0,
+        1, 2, 15, 43, 34, 2, 12, 2, 3,
+        1, 0, 12, 3, 0, 0, 21, 2, 2],
+    };
+  }
+  render() {
+    return (
+      <div className="App" >
+        <Mapa poblacion={this.state.poblacion} />
+      </div>
+    );
+  }
+}
+
+export default App;
